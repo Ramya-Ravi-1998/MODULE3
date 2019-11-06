8c4074543a844a32ef24112233981290b6beef40
Ramya-Ravi-1998 <55866250+Ramya-Ravi-1998@users.noreply.github.com>
2019-11-06 11:40:50 +0530

    Add files via upload

---
+++ SpringBootDemo123/src/main/java/spring/example/SpringBootDemo123/SpringBootDemo123Application.java
@@ -0,0 +1,13 @@
+package spring.example.SpringBootDemo123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo123Application.class, args);
+	}
+
+}
---
+++ SpringBootDemo123/src/test/java/spring/example/SpringBootDemo123/SpringBootDemo123ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.example.SpringBootDemo123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
